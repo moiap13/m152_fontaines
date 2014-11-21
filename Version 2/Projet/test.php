bcb4133e64554dce41dfa8fce5025b1228e4a671
moiap13 <moiap13@gmail.com>
2014-11-21 17:17:05 +0100

    Login Sucessful inscription sucessful déconnexion sucessful

---
+++ Version 2/Projet/test.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include 'pages/fonctions.php';
+var_dump_pre($_SESSION);
+
+echo '<a href="index.php">go</a>';
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
